c55870d6f6dd19fbf992a6e23d82e50c8cd18fea
Dinazns <dinaz2000@hotmail.fr>
2024-12-01 20:06:46 +0100

    Rectification du bug connexion côté administration

--- src/views/LoginUI.js
+++ src/views/LoginUI.js
@@ -17,7 +17,7 @@ export default () => {
             <div class="card">
               <div class="card-body">
                 <form class="form-signin" data-testid="form-employee">
-                  <h2 class="h3 mb-3 font-weight-normal">Employé</h1>
+                  <h2 class="h3 mb-3 font-weight-normal">Employé</h2>
                   <label for="inputEmail">Votre email</label>
                   <input type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
                   <label for="inputPassword">Mot de passe</label>
@@ -31,12 +31,12 @@ export default () => {
             <div class="card">
               <div class="card-body">
                 <form class="form-signin" data-testid="form-admin">
-                  <h2 class="h3 mb-3 font-weight-normal">Administration</h1>
+                  <h2 class="h3 mb-3 font-weight-normal">Administration</h2>
                   <label for="inputEmail">Votre email</label>
                   <input type="email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
                   <label for="inputPassword">Mot de passe</label>
                   <input type="password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
-                  <button  type="submit" class="btn btn-lg btn-primary btn-block" data-testid="admin-login-button" style="background-color: #0E5AE5;">Se connecter</button>
+                  <button class="btn btn-lg btn-primary btn-block" data-testid="admin-login-button" style="background-color: #0E5AE5;" type="submit">Se connecter</button>
                 </form>
               </div>
             </div>
